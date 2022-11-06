0e5706cd32b421f7e01c68b10c6f22638cf72af4
saurabh24gope <saurabhgope@gmail.com>
2022-11-06 10:42:31 +0530

    Git first project added

---
+++ BikkadIT-04-Clone-Project/src/test/java/com/BikkadIT/clone/BikkadIt04CloneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikkadIt04CloneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
