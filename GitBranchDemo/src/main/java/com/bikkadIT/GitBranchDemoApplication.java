e409ef97c662f09cddbde1d61a615dfe7a5c7828
saurabh24gope <saurabhgope@gmail.com>
2022-11-14 21:36:00 +0530

    Project added to repo

--- GitBranchDemo/src/main/java/com/bikkadIT/GitBranchDemoApplication.java
+++ GitBranchDemo/src/main/java/com/bikkadIT/GitBranchDemoApplication.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.bikkadIT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GitBranchDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GitBranchDemoApplication.class, args);
 	}
 
 }
--- GitBranchDemo/src/test/java/com/bikkadIT/GitBranchDemoApplicationTests.java
+++ GitBranchDemo/src/test/java/com/bikkadIT/GitBranchDemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.bikkadIT;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class GitBranchDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
